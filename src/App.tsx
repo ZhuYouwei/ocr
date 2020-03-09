ddb2bc4ee8036df852ad60bab9e644dff26a5b9c
Zhu Youwei <zhu.youwei@gmail.com>
2020-03-10 01:36:11 +0800

    image upload with preview

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import * as React from "react";
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import { ImageUpload } from "./ImageUpload";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+export const App = () => {
+  const [file, setFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("https://api.mydomain.com").then(response =>
+        console.log("hishishi", response)
+      );
+    };
+    fetchData();
+  }, [file]);
+
+  return <ImageUpload onFileUpload={setFile} />;
+};
---
+++ src/ImageUpload.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./styles.css";
+import { Upload, message } from "antd";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+}
+
+export class ImageUpload extends React.Component<{
+  onFileUpload: (file: File) => void;
+}> {
+  state = {
+    loading: false
+  };
+
+  handleChange = info => {
+    if (info.file.status === "uploading") {
+      this.setState({ loading: true });
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, imageUrl =>
+        this.setState({
+          imageUrl,
+          loading: false
+        })
+      );
+      this.props.onFileUpload(info.file.originFileObj);
+    }
+  };
+
+  render() {
+    const uploadButton = (
+      <div>
+        {this.state.loading ? <LoadingOutlined /> : <PlusOutlined />}
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    const { imageUrl } = this.state;
+    return (
+      <Upload
+        name="random"
+        listType="picture-card"
+        className="avatar-uploader"
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        showUploadList={false}
+        beforeUpload={beforeUpload}
+        onChange={this.handleChange}
+      >
+        {imageUrl ? (
+          <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+        ) : (
+          uploadButton
+        )}
+      </Upload>
+    );
+  }
+}
